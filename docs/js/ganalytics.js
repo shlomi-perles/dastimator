3882a21a79481f937a9d44532959161a169a329d
shlomi-perles <shlomip100@gmail.com>
2023-05-24 13:49:13 +0300

    Fix lecture issues

---
+++ docs/js/ganalytics.js
@@ -0,0 +1,12 @@
+var imported = document.createElement('script');
+imported.src = 'https://www.googletagmanager.com/gtag/js?id=G-XK2K713J23';
+document.head.appendChild(imported);
+
+window.dataLayer = window.dataLayer || [];
+
+function gtag() {
+    dataLayer.push(arguments);
+}
+
+gtag('js', new Date());
+gtag('config', 'G-XK2K713J23');
